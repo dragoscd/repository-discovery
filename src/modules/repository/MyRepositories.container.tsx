8a41bd5a319127313e604777d6ecd3abbf206ab3
Dragos Dumitriu <dragosc.dumitriu@gmail.com>
2021-02-01 00:44:57 +0100

    feat(repositories): update only outdated repositories

--- src/modules/repository/MyRepositories.container.tsx
+++ src/modules/repository/MyRepositories.container.tsx
@@ -1,10 +1,12 @@
+import differenceInMinutes from 'date-fns/fp/differenceInMinutes';
 import React, { useEffect } from 'react';
-import useFetch, { CachePolicies } from 'use-http';
+import useFetch from 'use-http';
 import CardList from '../../components/CardList';
 import { IRepository } from '../../types/common';
 import { useMyRepositories } from './MyRepositories.context';
 
 const BATCH_SIZE = 2;
+const OUTDATE_TOLLERANCE_MINUTES = 5;
 
 const MyRepositoriesContainer: React.FC = () => {
   const {
@@ -12,7 +14,7 @@ const MyRepositoriesContainer: React.FC = () => {
     updateRepository,
     removeRepository,
   } = useMyRepositories();
-  const { get } = useFetch({ cachePolicy: CachePolicies.NETWORK_ONLY });
+  const { get } = useFetch();
 
   const repositories = Object.keys(starredRepositories).map(
     key => starredRepositories[key].repository
@@ -29,9 +31,21 @@ const MyRepositoriesContainer: React.FC = () => {
   };
 
   useEffect(() => {
-    const batchedRepositories = repositories
+    const repositoriesToUpdate = Object.keys(starredRepositories)
+      .filter(
+        key =>
+          differenceInMinutes(
+            new Date(starredRepositories[key].lastUpdated),
+            new Date()
+          ) >= OUTDATE_TOLLERANCE_MINUTES
+      )
+      .map(key => starredRepositories[key].repository);
+
+    console.log(repositoriesToUpdate);
+
+    const batchedRepositories = repositoriesToUpdate
       .map((_, i) =>
-        i % BATCH_SIZE ? null : repositories.slice(i, i + BATCH_SIZE)
+        i % BATCH_SIZE ? null : repositoriesToUpdate.slice(i, i + BATCH_SIZE)
       )
       .filter(batch => batch);
 
